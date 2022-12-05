47233b17fdd7225fc066efec7905ab4f4c9eae49
kianlak <lak.kian.ca@gmail.com>
2022-12-05 12:27:34 -0600

    Final

--- Day5/Day5Part1.py
+++ Day5/Day5Part1.py
@@ -1,16 +1,18 @@
 import re
 
+# This will move crates based on the instructions one by one
+
 def part1(crate_stacks, top_crates):
     # Read instructions from instructions.txt
     with open('instructions.txt') as file:
-        # Reads each instruction individually line by line
         for line in file.readlines():
             num_crate, from_stack, to_stack = re.findall(r'\d+', line)  # Extract necessary values to respective variables          
             num_crate, from_stack, to_stack = int(num_crate), int(from_stack) - 1, int(to_stack) - 1
                     
             for x in range(num_crate):
-                crate_stacks[to_stack].append(crate_stacks[from_stack].pop())
-            
+                crate_stacks[to_stack].append(crate_stacks[from_stack].pop())   # Take each crate off a stack and put 
+                                                                                # them onto another stack
+                                                                                
     for x in range(9):
         top_crates += crate_stacks[x].pop()
 
--- Day5/Day5Part2.py
+++ Day5/Day5Part2.py
@@ -1,20 +1,21 @@
 import re
 
+# This will take multiple crates and move them at once
+
 def part2(crate_stacks, top_crates):
     # Read instructions from instructions.txt
     with open('instructions.txt') as file:
-        # Reads each instruction individually line by line
         for line in file.readlines():
             num_crate, from_stack, to_stack = re.findall(r'\d+', line)  # Extract necessary values to respective variables          
             num_crate, from_stack, to_stack = int(num_crate), int(from_stack) - 1, int(to_stack) - 1
             
-            crate_in_order = []
+            crate_in_order = [] # Use for holding the crates temporarily
             
             for x in range(num_crate):
                 crate_in_order.append(crate_stacks[from_stack].pop())
 
-            crate_in_order.reverse()
-            crate_stacks[to_stack].extend(crate_in_order)
+            crate_in_order.reverse()                        # Reverse to put crates in order
+            crate_stacks[to_stack].extend(crate_in_order)   # Merges crate_stacks[to_stack] with crate_in_order
 
     for x in range(9):
         top_crates += crate_stacks[x].pop()
